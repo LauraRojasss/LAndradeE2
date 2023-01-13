817b1b6ab88eb3f315d8faa0db87383b5a1d5dfa
Laura Rojas <902andradelaura@gmail.com>
2023-01-12 19:57:16 -0500

    Creación del proyecto

---
+++ citas-reat/src/App.jsx
@@ -0,0 +1,17 @@
+/*Función creadora de la aplicación*/
+
+function App() {
+
+
+  return (
+
+    /*Etiqueta padre y el body de la página*/
+    <>
+    
+    
+    </>
+  )
+}
+/*Export para importar el módulo a otras partes de la app*/
+
+export default App
