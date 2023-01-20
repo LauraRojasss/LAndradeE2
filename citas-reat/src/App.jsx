1385a2c85ea6fab35cfd1360c511ea2c4acf38c3
Laura Rojas <902andradelaura@gmail.com>
2023-01-19 19:47:41 -0500

    Hook

--- citas-reat/src/App.jsx
+++ citas-reat/src/App.jsx
@@ -3,6 +3,7 @@ import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListaPacientes from "./components/ListaPacientes"
 
+
 /*Función creadora de la aplicación*/
 
 function App() {
@@ -11,10 +12,10 @@ function App() {
   return (
 
     /*Etiqueta padre y el body de la página*/
-    <div className="container mx-auto mt-20">
+    <div className="container mx-auto mt-20" >
       <Header/>
-      <div className="mt-12 md:flex">
-          <Formulario/>
+      <div className="mt-12 md:flex"> 
+          <Formulario/> 
           <ListaPacientes/>
           
       </div>
