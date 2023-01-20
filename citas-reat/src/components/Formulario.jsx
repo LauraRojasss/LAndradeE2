3cf6d0f85bc93d981456e278d73cb88718e490fa
Laura Rojas <902andradelaura@gmail.com>
2023-01-19 19:47:59 -0500

    Hooks

--- citas-reat/src/components/Formulario.jsx
+++ citas-reat/src/components/Formulario.jsx
@@ -1,14 +1,41 @@
 import { useState, useEffect } from "react";
 
 const Formulario = () => {
-  const [nombre, setNombre] = useState('');
+  const [nombre, setNombre] = useState("");
+  const [propietario, setPropietario] = useState("");
+  const [email, setEmail] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [sintomas, setSintomas] = useState("");
+
+  //Hook para capturar errores.
+  const [error, setError] = useState(false);
+
+  //Limpiar o resetar el formulario cuando carga la página
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log('Enviando formulario...')
+
+
+      //Validar formulario para que no vaya con campos vacios.
+
+  if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+    setError(true);
+    return;
+  }
+
+  setError(false);
+
+  };
+
+
 
   //setNombre('Trosky');
 
   //console.log(nombre);
 
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">
         Seguimiento a Pacientes
       </h2>
@@ -18,7 +45,16 @@ const Formulario = () => {
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+        onSubmit={handleSubmit}
+      >
+        {error && (
+          <div className="bg-red-800 text-white p-3 uppercase text-center font-bold rounded-md">
+            <p>Todos los campos son obligatorios</p>
+          </div>
+        )}
+
         <div className="mb-5">
           <label
             htmlFor="mascota"
@@ -32,15 +68,14 @@ const Formulario = () => {
             type="text"
             className="border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
             placeholder="Ingrese el nombre de la mascota"
-
-            value={nombre} onChange={(e)=> console.log(setNombre(e.target.value))}
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
-          
         </div>
 
         <div className="mb-5">
           <label
-            htmlFor="mpropietario"
+            htmlFor="propietario"
             className="block text-gray-700 uppercase font-bold"
           >
             Nombre del Propietario:
@@ -51,6 +86,8 @@ const Formulario = () => {
             type="text"
             className="border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
             placeholder="Ingrese el nombre del propietario"
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
           />
         </div>
 
@@ -67,6 +104,8 @@ const Formulario = () => {
             type="email"
             className="border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
             placeholder="Ingrese el email del propietario"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -83,22 +122,26 @@ const Formulario = () => {
             type="date"
             className="border-2 w-full p-2 mt-2 rounded-md"
             placeholder="Ingrese la fecha de ingreso"
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
           />
         </div>
 
         <div className="mb-5">
           <label
-            htmlFor="fecha"
+            htmlFor="sintomas"
             className="block text-gray-700 uppercase font-bold"
           >
             Sintomas :
           </label>
 
           <textarea
-            id="fecha"
+            id="sintomas"
             type="text"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             placeholder="Ingrese los síntomas de la mascota"
+            value={sintomas}
+            onChange={(e) => setSintomas(e.target.value)}
           />
         </div>
 
